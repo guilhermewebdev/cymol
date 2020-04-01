cbe346344ee8943a21580a378c761be0c4bd23db
Guilherme Isaías Silva <guilherme@localhost.localdomain>
2020-04-01 04:30:32 -0300

    criação do servidor gateway

---
+++ cymol/server/api/gateway/src/main.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
+
+// Initialize an ApolloGateway instance and pass it an array of
+// your implementing service names and URLs
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: 'accounts', url: 'http://localhost:4001' },
+    // Define additional services here
+  ],
+});
+
+// Pass the ApolloGateway to the ApolloServer constructor
+const server = new ApolloServer({
+  gateway,
+
+  // Disable subscriptions (not currently supported with ApolloGateway)
+  subscriptions: false,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
