4b97224907f016b8fd4ca8cbeba279cca1b4ad89
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 00:15:25 -0300

    Criação da classe dos produtos

--- cymol/server/api/product/models.py
+++ cymol/server/api/product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    price = models.FloatField(
+        verbose_name='price',
+        null=True,
+    )
+    name = models.CharField(
+        max_length=100,
+    )
+    description = models.CharField(
+        max_length=1000,
+    )
+    stock = models.IntegerField(
+        verbose_name='stock amount',
+        default=1,
+    )
