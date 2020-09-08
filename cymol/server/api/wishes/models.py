998a8b89ba805ae71ecc21ad2e46523b46d6ad67
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 19:32:15 -0300

    Modelagem da lista de desejos

--- cymol/server/api/wishes/models.py
+++ cymol/server/api/wishes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Wish(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='wishes',
+    )
+    product = models.ForeignKey(
+        'product.Product',
+        on_delete=models.CASCADE,
+        related_name='wishes',
+    )
+    registration_date = models.DateTimeField(
+        verbose_name='registration date',
+        auto_now=True,
+    )
