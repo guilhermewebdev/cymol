979807806d8a6ea7a432861ce2822a133e71c95e
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 00:23:55 -0300

    Registro dos produtos no admin

--- cymol/server/api/product/admin.py
+++ cymol/server/api/product/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from api.admin import register, cymol
+from . import models
+
+class ProductImageAdminInline(admin.TabularInline):
+    extra = 1
+    model = models.ProductImage
+
+@register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdminInline]
