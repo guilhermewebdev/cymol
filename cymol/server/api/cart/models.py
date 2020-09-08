06efa5d6d7b384911f9784cf42ab380c310abfd4
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 19:56:57 -0300

    Modelagem do carrinho de compras

--- cymol/server/api/cart/models.py
+++ cymol/server/api/cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='cart'
+    )
+    products = models.ManyToManyField(
+        'product.Product',
+        related_name='carts',
+    )
