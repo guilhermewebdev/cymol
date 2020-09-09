574c5864e2e18b502a55a6274f8fca093a372708
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 21:37:03 -0300

    Implementação do admin nas compras

--- cymol/server/api/order/admin.py
+++ cymol/server/api/order/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api.admin import cymol, register
+from . import models
 
-# Register your models here.
+@register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
