0fb62fd664576c82ee4fd322a282fd3068b8d002
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 21:22:41 -0300

    Criação do modelo de pagamento

--- cymol/server/api/payment/models.py
+++ cymol/server/api/payment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Payment(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.DO_NOTHING,
+        related_name='payments',
+    )
+    value = models.FloatField(
+        verbose_name='value',
+    )
