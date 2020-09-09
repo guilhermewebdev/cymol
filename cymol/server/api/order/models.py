fe981ac19a6959423b13bd11699ee1f0c9da27aa
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-09-08 21:35:41 -0300

    Modelagem da compra

--- cymol/server/api/order/models.py
+++ cymol/server/api/order/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.DO_NOTHING,
+        related_name='orders',
+    )
+    products = models.ManyToManyField(
+        'product.Product',
+        related_name='orders'
+    )
+    registration_date = models.DateTimeField(
+        auto_now=True,
+    )
